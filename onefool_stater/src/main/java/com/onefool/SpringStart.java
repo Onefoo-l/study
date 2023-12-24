3283d3ebb3eacfc57ba79cf70764cdc8bccb8974
Onefoo_l <974362219@qq.com>
2023-12-24 10:23:52 +0800

    初始化模块

---
+++ onefool_stater/src/main/java/com/onefool/SpringStart.java
@@ -0,0 +1,22 @@
+package com.onefool;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author linjiawei
+ * @Date 2023/12/24 9:06
+ */
+@SpringBootApplication
+public class SpringStart {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringStart.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStart.class,args);
+        LOGGER.info("项目启动成功!!");
+    }
+}
